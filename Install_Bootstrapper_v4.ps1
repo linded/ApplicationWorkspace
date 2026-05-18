237fb6db86a3aa94f45f0e25f67121b886a6c220
Donny Van der Linde <93385046+linded@users.noreply.github.com>
2026-05-18 16:20:54 +0200

    Add files via upload

---
+++ Install_Bootstrapper_v4.ps1
@@ -0,0 +1,29 @@
+﻿# Set variables
+$destinationFolder = "C:\Temp\Liquit\"
+$url = "https://download.liquit.com/extra/Bootstrapper/"
+$container_url = "https://stliquitprodwe01.blob.core.windows.net/donny/"
+$sas_token = "?sp=r&st=2025-01-08T15:26:30Z&se=2026-12-31T23:26:30Z&spr=https&sv=2022-11-02&sr=c&sig=YJjupGG%2BqaXXhvQ10erGrHG3Dm8XaVEaE80bNHUOl0o%3D"
+
+$container = "AVD/"
+$strapper = "AgentBootstrapper-Win-4.4.4130.3708.exe"
+$cert = "AgentRegistration.cer"
+$agent = "Agent.json"
+
+# Create folder C:\Temp\Liquit 
+If (-not (Test-Path $destinationFolder)) {
+    New-Item -Path $destinationFolder -ItemType Directory -Force
+}
+
+# Download the files
+Invoke-WebRequest -Uri ($url + $strapper) -OutFile ($destinationFolder + $strapper)
+Invoke-WebRequest -Uri ($container_url + $container + $cert + $sas_token) -OutFile ($destinationFolder + $cert)
+Invoke-WebRequest -Uri ($container_url + $container + $agent + $sas_token) -OutFile ($destinationFolder + $agent)
+
+# Install with Liqut Agent BootStrapper
+Set-Location $destinationFolder
+.\AgentBootstrapper-Win-4.4.4130.3708.exe --zone "https://donny.liquit.com/" --registrationType Certificate --logPath ".\Install" --wait --certificate ".\AgentRegistration.cer" --startDeployment --deployment "MS Intune Deployment"
+
+
+
+
+
